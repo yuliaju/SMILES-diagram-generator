9e25b0a19418a86708f683541f508be23463b275
Julia Yu <yuliaju2@gmail.com>
2016-11-09 11:12:08 -0500

    Small refactors: made data types instances of Show, made isBond pointfree, added local definitions to removePivotChar

--- sdg.hs
+++ sdg.hs
@@ -12,11 +12,23 @@ data Bond   = Single
             | Double
             | Triple
             | Aromatic
-    deriving (Show, Eq)
+    deriving (Eq)
+
+instance Show Bond where
+  show Single    = "-"
+  show Double    = "="
+  show Triple    = "#"
+  show Aromatic  = ":"
+
 data Token  = TokBond Bond
             | TokAtom String
             | TokBranch [Token]
-    deriving (Show, Eq)
+    deriving (Eq)
+
+instance Show Token where
+  show (TokBond bond) = show bond
+  show (TokAtom atom) = atom
+  show (TokBranch tokens) = "( " ++ (concatTokenStrs $ fmap show tokens) ++ " )"
 
 -- lexer
 tokenize :: String -> [Token]
@@ -29,16 +41,19 @@ tokenize (c : rest)
     else
       if isLower (head rest) then TokAtom [c, head rest] : tokenize rest
       else TokAtom [c] : tokenize rest
+  | otherwise = error $ "Cannot tokenize " ++ [c]
   where splitRest = removePivotChar ')' rest
 
 removePivotChar :: Char -> String -> (String, String)
 remotePivotChar _ [] = []
 removePivotChar c str = (front, back)
   where front = takeWhile (/= c) str
-        back  = if (length (dropWhile (/= c) str) > 1) then tail $ dropWhile (/= c) str
+        back  = if (lenAfterC > 1) then tail afterC
                 else
-                  if (length (dropWhile (/= c) str) == 1) then []
+                  if (lenAfterC == 1) then []
                   else error "Unterminated branch in input"
+        afterC      = dropWhile (/= c) str
+        lenAfterC   = length afterC
 
 toBond :: Char -> Bond
 toBond c
@@ -47,24 +62,13 @@ toBond c
   | c == '#' = Triple
   | c == ':' = Aromatic
 
-showContent :: Token -> String
-showContent (TokBond bond) = bondToStr bond
-showContent (TokAtom atom) = atom
-showContent (TokBranch tokens) = "( " ++ (concatTokenStrs $ fmap showContent tokens) ++ " )"
-
 concatTokenStrs :: [String] -> String
 concatTokenStrs = unwords
 
 isBond :: Char -> Bool
-isBond c = c `elem` "-=#:"
-
-bondToStr :: Bond -> String
-bondToStr Single    = "-"
-bondToStr Double    = "="
-bondToStr Triple    = "#"
-bondToStr Aromatic  = ":"
+isBond = (`elem` "-=#:")
 
 main :: IO ()
 main = do
   smiles <- getLine
-  putStrLn $ concatTokenStrs $ fmap showContent $ tokenize smiles
+  putStrLn $ concatTokenStrs $ fmap show $ tokenize smiles
